c67e0518a03e2b7fdb3e60a9c1ed2a412cc940eb
Tilak Thapa <contact@jrtilak.dev>
2025-12-05 21:22:33 +0545

    restructure

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function HomePage() {
-  // This will be handled by middleware
-  // If not logged in -> /auth/login
-  // If logged in -> /home
-  redirect("/home");
-}
