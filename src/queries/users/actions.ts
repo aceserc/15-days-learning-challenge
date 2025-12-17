e3c116f08a67213a7014a752221404b937734cc1
Sandip Sapkota <sandipsapkota9844@gmail.com>
2025-12-17 12:15:33 +0545

    fixed deployment issue

--- src/queries/users/actions.ts
+++ src/queries/users/actions.ts
@@ -1,8 +1,7 @@
 "use server";
 
-import { CHALLANGE_DATA } from "@/content/data";
 import { db } from "@/db";
-import { participants, users } from "@/db/schema";
+import { users } from "@/db/schema";
 import { auth } from "@/lib/auth";
 import { eq } from "drizzle-orm";
 import { tryCatchAction } from "../lib";
@@ -22,18 +21,11 @@ export const fetchCurrentUser = tryCatchAction(async (): Promise<ActionResponse<
     throw new Error("User not found");
   }
 
-  const participant = await db.select({
-    participants: participants,
-    users: users
-  }).from(participants).leftJoin(users, eq(participants.userId, users.id)).where(eq(participants.techfestId, CHALLANGE_DATA.techfestId)).limit(1)
-  console.log(participant, "from server")
-
   return {
     success: true,
     message: "User profile fetched",
     data: {
       user,
-      participant,
     },
   };
 })
